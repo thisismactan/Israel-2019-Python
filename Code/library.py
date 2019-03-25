9bf5a781fa7b5a169e5b69bd5b52da4c71919ee7
Mac Tan <mactan@gmail.com>
2019-03-25 14:56:22 -0400

    Removed (outdated) wtd_var function

--- Code/library.py
+++ Code/library.py
@@ -20,20 +20,3 @@ def pctile(n):
         return np.percentile(x, n)
     pctile_.__name__ = 'pctile_%s' % n
     return pctile_
-
-def wtd_var(x, weights = None):
-    """ Returns the weighted variance of x, weighted by 'weights' argument.
-    Defaults to unweighted variance. """
-    ## Convert to nd.arrays
-    x = np.array(x)
-    if weights == None:
-        weights = np.array(weights)
-    
-    w_mean = np.average(x, weights = weights)
-    dev_sq = (x - w_mean) ** 2
-    if weights == None:
-        return dev_sq.sum()/(dev_sq.size - 1)
-    else:
-        n = sum(weights != 0)
-        w_var = sum(dev_sq) / ((dev_sq.size - 1) * sum(weights))
-        return w_var
